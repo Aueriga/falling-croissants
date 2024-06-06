4ad47aa849a9b026ec33651db7a459903d4006e6
Ayman Alkhatib <aemn22384@gmail.com>
2024-06-07 00:08:21 +0200

    add click event and score element

--- script.js
+++ script.js
@@ -7,18 +7,25 @@ document.addEventListener('DOMContentLoaded', () => {
         croissant.src = croissantSrc;
         croissant.classList.add('croissant');
         croissant.style.left = Math.random() * window.innerWidth + 'px';
-        croissant.style.animationDuration = 3 + Math.random() * 5 + 's';
+        croissant.style.animationDuration = 12 + Math.random() * 5 + 's';
         container.appendChild(croissant);
 
-        
+
         croissant.addEventListener('animationend', () => {
             container.removeChild(croissant);
         });
     }
 
     function startRain() {
-        setInterval(createCroissant, 300); 
+        setInterval(createCroissant, 900);
     }
 
     startRain();
 });
+const score = document.querySelector(".score span")
+addEventListener("click", (e) => {
+    if (e.target.className === "croissant") {
+        score.textContent++
+        e.target.style = { display: "none" }
+    }
+})
