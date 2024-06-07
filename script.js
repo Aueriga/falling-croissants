aa3d0dc093690e7ffa30484391ca3a2262d34df1
Ayman Alkhatib <aemn22384@gmail.com>
2024-06-07 15:05:31 +0200

    add failed feature

--- script.js
+++ script.js
@@ -18,36 +18,56 @@ const sweets = {
 
 }
 let current_score = 0
+let failed = false
 let sweets_list = Object.keys(sweets)
+let sweetsIntervId;
 
 const container = document.getElementById('container');
 const score = document.querySelector(".score span")
 
 function createSweet() {
     let random_item = sweets_list[Math.floor(Math.random() * sweets_list.length)]
-    const imgSrc = `./assets/${random_item}.${sweets[random_item].filetype}`;
+    const imgSrc = `./media/assets/${random_item}.${sweets[random_item].filetype}`;
     const sweet = document.createElement('img');
 
     sweet.src = imgSrc;
     sweet.classList.add(random_item);
     sweet.classList.add('sweet');
     sweet.style.left = Math.random() * window.innerWidth + 'px';
-    sweet.style.animationDuration = 12 + Math.random() * 5 + 's';
+    sweet.style.animationDuration = 12 + Math.random() - (current_score * 0.005) * 5 + 's';
 
     container.appendChild(sweet);
     sweet.addEventListener('animationend', () => {
-        container.removeChild(sweet);
+        handlefailed()
     });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-
     function startRain() {
-        setInterval(createSweet, 900);
+        sweetsIntervId = setInterval(createSweet, 900);
     }
-
     startRain();
 });
+
+function handlefailed() {
+    document.body.removeChild(container)
+    clearInterval(sweetsIntervId)
+    const failDiv = document.createElement("div")
+    const tryAgainBtn = document.createElement("button")
+    const scoreSpan = document.createElement("span")
+
+    scoreSpan.textContent = `Your score: ${current_score}`
+    failDiv.className = "failed"
+
+    tryAgainBtn.textContent = "Try Again"
+    tryAgainBtn.onclick = () => {
+        location.reload()
+    }
+    failDiv.appendChild(scoreSpan)
+    failDiv.appendChild(tryAgainBtn)
+
+    document.body.appendChild(failDiv)
+}
 
 
 addEventListener("click", (e) => {
